27bdd545e42d952d54f9b7c549f6ad955ad0206c
SamuelSlavik <samuel.slavik@protonmail.com>
2020-03-08 19:49:39 +0100

    i am working

--- js/getMedia.js
+++ js/getMedia.js
@@ -1,23 +1,9 @@
 let constraints = { audio: true, video: { width: 1280, height: 720 } };
-/*
-async function getMedia(constraints) {
-    let stream = null;
 
-    try {
-        stream = await navigator.mediaDevices.getUserMedia(constraints);
-        /* use the stream *//*
-    } catch(err) {
-        /* handle the error *//*
-    }
-}*/
 function getMedia() {
     navigator.mediaDevices.getUserMedia(constraints)
         .then(function (mediaStream) {
-            var video = document.querySelector('video');
-            video.srcObject = mediaStream;
-            video.onloadedmetadata = function (e) {
-                video.play();
-            };
+            // sem zadaj co chces urobit s outputom
         })
         .catch(function (err) {
             console.log(err.name + ": " + err.message);
